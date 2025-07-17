93309c6d7a3c16bd4cd141ca773bc0c0ccd7cc77
ALVIAN SYAH BURHANI <105841103522@student.unismuh.ac.id>
2025-07-17 14:05:19 +0800

    4terbaru

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    winkyRough: require("../assets/fonts/WinkyRough.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && error) {
+    return null; // tetap tampilkan splash screen sampai font siap
+  }
+
   return <Stack />;
 }
